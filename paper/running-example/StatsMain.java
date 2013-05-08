14b3f3c349a660dc9fe8903691cc607ef1040c2e
Marc-André Laverdière <marc-andre@atc.tcs.com>
2013-05-08 14:46:23 -0400

    Added measuring of SootClass objects

--- paper/running-example/StatsMain.java
+++ paper/running-example/StatsMain.java
@@ -1,3 +1,5 @@
+import com.javamex.classmexer.MemoryUtil;
+import org.apache.commons.io.FileUtils;
 import soot.*;
 
 import java.util.Map;
@@ -9,6 +11,9 @@ public class StatsMain {
     PackManager.v().getPack("wjtp").add(new Transform("wjtp.stats", new SceneTransformer() {
       @Override
       protected void internalTransform(String phaseName, Map options) {
+        long memoryUsed = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+        long memoryUsedClasses = MemoryUtil.deepMemoryUsageOfAll(Scene.v().getClasses(), MemoryUtil.VisibilityFilter.ALL);
+
         int resolvedBodies =0, resolvedSignatures= 0 , resolvedHierarchy = 0, dandling = 0;
         int numMethods = 0;
 
@@ -29,11 +34,14 @@ public class StatsMain {
         }
 
         G.v().out.println("Dandling: " + dandling + " Hierarchy: " + resolvedHierarchy + " Signatures: " + resolvedSignatures + " Bodies: "+ resolvedBodies +". Num SootMethod: "+ numMethods);
+        G.v().out.println("Memory used in wjtp phase: " + memoryUsed / (float) FileUtils.ONE_MB + " Mb\tMemory used for SootClass objects: "
+                + memoryUsedClasses / (float)FileUtils.ONE_MB + " Mb");
+
       }
     }));
 
     soot.Main.main(new String[]{
-            "-w","-prepend-classpath", "-output-format", "none", "-soot-classpath", args[0], "A"
+           "-time", "-w","-prepend-classpath", "-output-format", "none", "-soot-classpath", args[0], "A"
     });
   }
 }
